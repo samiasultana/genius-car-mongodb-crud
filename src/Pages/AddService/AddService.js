523f50bedba2e5f9764379c69fd89a1ced5c2c9b
unknown <samia361@gmail.com>
2021-11-18 21:04:27 +0600

    final

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,11 +1,26 @@
+import axios from 'axios';
 import React from 'react';
 import { useForm } from "react-hook-form";
 import "./AddService.css";
 
 const AddService = () => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const { register, handleSubmit,reset  } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        // now copy from axios
 
+        axios.post('http://localhost:5000/services', data)
+            .then(function (response) {
+                if(response.data.insertedId){
+                    alert("Adding Successful")
+                    reset();
+                }
+            })
+            // .catch(function (error) {
+            //     console.log(error);
+            // });
+
+    }
     return (
         <div className="add-service">
             <h2>Add a service</h2>
@@ -13,8 +28,8 @@ const AddService = () => {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <input {...register("name", { required: true, maxLength: 20 })} placeholder="Name" />
                 <textarea {...register("description")} placeholder='Description' />
-                <input type="number" {...register("price")} placeholder="price"/>
-                <input {...register("img")} placeholder="img url"/>
+                <input type="number" {...register("price")} placeholder="price" />
+                <input {...register("img")} placeholder="img url" />
                 <input type="submit" />
             </form>
         </div>
